0b41425e8de4e7e221eaa0e991cc73ba6fdc16e6
snowyMountain98 <sarahfox219@gmail.com>
2024-11-09 23:39:27 +0900

    헐랭

--- escapeRoomsPlanet.py
+++ escapeRoomsPlanet.py
@@ -21,7 +21,7 @@ driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://xn--2e0b040a4xj.com/reservation?branch=4&theme=12&date=2024-11-14#list") # 날짜 수정
 
 # 웹페이지가 로드될 때까지 2초를 대기
-driver.implicitly_wait(time_to_wait=2)  
+driver.implicitly_wait(5)  
 
 # 시간 선택
 # 17:40
@@ -41,9 +41,9 @@ elif driver.find_element(By.XPATH, '//*[@id="wrap"]/div/div[2]/section/div/ul/li
     driver.find_element(By.XPATH,'//*[@id="wrap"]/div/div[2]/section/div/ul/li[9]/div/button').click()
 
 # 성함 입력
-driver.find_element(By.XPATH,'//*[@id="wrap"]/form/div[1]/article/section[1]/div/table/tbody/tr[3]/td/input').send_keys('이서라')
+driver.find_element(By.XPATH,'//*[@id="wrap"]/form/div[1]/article/section[1]/div/table/tbody/tr[3]/td/input').send_keys('홍길동')
 # 연락처 입력
-driver.find_element(By.XPATH,'//*[@id="wrap"]/form/div[1]/article/section[1]/div/table/tbody/tr[4]/td/input').send_keys('010-8821-8986')
+driver.find_element(By.XPATH,'//*[@id="wrap"]/form/div[1]/article/section[1]/div/table/tbody/tr[4]/td/input').send_keys('010-1234-5678')
 # 예약 인원 선택
 Select(driver.find_element(By.XPATH, '//*[@id="evePeople"]')).select_by_value('4')
 # 결제방식 선택
@@ -52,4 +52,4 @@ driver.find_element(By.XPATH,'//*[@id="wrap"]/form/div[1]/article/section[1]/div
 driver.find_element(By.XPATH,'//*[@id="wrap"]/form/div[1]/article/section[1]/div/div/label').click()
 
 # 예약히가 버튼 클릭
-# driver.find_element(By.XPATH,'//*[@id="eveReservationBtn"]').click()
+driver.find_element(By.XPATH,'//*[@id="eveReservationBtn"]').click()
